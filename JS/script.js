73203a74f9220d3ca450866effc06a478532a68a
pagos21 <paga@localhost.yeah>
2020-06-30 18:09:45 +0200

    v 1.2

--- JS/script.js
+++ JS/script.js
@@ -21,8 +21,8 @@ var arrCPU = [];
 var arrUser = [];
 var score = 0;
 
-for (var i = 0; (arrCPU.length != 6); i++) {
-  var cpuNum = randomIntInclusiveF(1, 8);
+for (var i = 0; (arrCPU.length != 16); i++) {
+  var cpuNum = randomIntInclusiveF(1, 100);
   if (arrCPU.includes(cpuNum)) {
     continue;
   } else {
@@ -60,6 +60,7 @@ btn.addEventListener("click", function(){
     console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
     alert("End of the line dude!" + String.fromCodePoint(0x1F4A3));
     console.log("Your score is " + score);
+    throw new Error("GAME OVER!")
   }
   // Se il il punteggio raggiunge 10
   else if (score >= 10) {
