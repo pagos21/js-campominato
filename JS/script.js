6188022f0ea9a4113137f271162d015c5e403c9d
pagos21 <paga@localhost.yeah>
2020-06-30 17:03:45 +0200

    v 1.1 (WIP + Comments)

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,7 @@ function randomIntInclusiveF(min, max) {
 
 // Check for duplicate numbers
 function duplicates(p1){
+  // Creo un nuovo Set dall'argomento(array) della funzione e confronto le loro lunghezze (TRUE se le lunghezze sono diverse).
   return new Set(p1).size != p1.length;
 }
 
@@ -45,27 +46,29 @@ console.log();
 btn.addEventListener("click", function(){
   var num = parseInt(document.getElementById("num").value);
 
+  // Controllo che il numero sia valido
   if (isNaN(num) || /\s/.test(num) || num > 100) {
     console.log("Numero non valido!");
   }
+  // Se il numero è gia stato inserito
   else if (arrUser.includes(num)) {
     alert("Numero già inserito!");
     console.log(arrUser);
-
   }
+  // Se il numero è presente nel campo minato della CPU
   else if (arrCPU.includes(num)) {
     console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
-    alert("End of the line dude!");
+    alert("End of the line dude!" + String.fromCodePoint(0x1F4A3));
     console.log("Your score is " + score);
   }
+  // Se il il punteggio raggiunge 10
   else if (score >= 10) {
     alert("Yeah, you win!!!" + String.fromCodePoint(0x1F91F))
   }
-
+  // Se il numero è valido aumento il punteggio
   else {
     arrUser.push(num);
     score += 1;
     console.log(String.fromCodePoint(0x1F44C));
   }
-
 });
