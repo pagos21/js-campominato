8cfc61aaa8efb24e423deea5e789786946c3bbb4
pagos21 <paga@localhost.yeah>
2020-07-01 13:16:16 +0200

    v 2.3

--- BONUS/JS/script.js
+++ BONUS/JS/script.js
@@ -5,66 +5,103 @@ function randomIntInclusiveF(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Il max è incluso e il min è incluso
 }
 
-// Check for duplicate numbers
-function duplicates(p1){
-  // Creo un nuovo Set dall'argomento(array) della funzione e confronto le loro lunghezze (TRUE se le lunghezze sono diverse).
-  return new Set(p1).size != p1.length;
-}
-
-
 function easy(){
   console.log("Easy mode!");
-  for (var i = 0; (arrCPU.length != 16); i++) {
+  for (var i = 0; (uniqueSet.size != 16); i++) {
     var cpuNum = randomIntInclusiveF(1, 100);
-    if (arrCPU.includes(cpuNum)) {
-      continue;
-    } else {
-    arrCPU.push(cpuNum);
-    }
-    // uniqueSet.add(cpuNum);
+    uniqueSet.add(cpuNum);
   }
-  console.log(arrCPU);
+  console.log(uniqueSet);
+
 }
 
 function medium(){
   console.log("Medium mode!");
-  for (var i = 0; (arrCPU.length != 16); i++) {
+  for (var i = 0; (uniqueSet.size != 16); i++) {
     var cpuNum = randomIntInclusiveF(1, 80);
-    if (arrCPU.includes(cpuNum)) {
-      continue;
-    } else {
-    arrCPU.push(cpuNum);
-    }
-    // uniqueSet.add(cpuNum);
+
+    uniqueSet.add(cpuNum);
   }
-  console.log(arrCPU);
+  console.log(uniqueSet);
+
 }
 
 function hard(){
   console.log("Hard mode!");
-  for (var i = 0; (arrCPU.length != 16); i++) {
+  for (var i = 0; (uniqueSet.size != 16); i++) {
     var cpuNum = randomIntInclusiveF(1, 50);
-    if (arrCPU.includes(cpuNum)) {
-      continue;
-    } else {
-    arrCPU.push(cpuNum);
-    }
-    // uniqueSet.add(cpuNum);
+    uniqueSet.add(cpuNum);
   }
-  console.log(arrCPU);
+  console.log(uniqueSet);
+
 }
 
 
+function enterkey(){
+  if (event.keyCode === 13) {
+    enterPressed();
+  }
+}
+
+function enterPressed(){
+  console.log(score);
+  var num = parseInt(document.getElementById("num").value);
+    // Controllo che il numero sia valido
+    if (difficultyG == "Easy" && num > 100) {
+      document.getElementById("list").innerHTML += "<li><i>" + " Wrong Input " + num + " Deve essere compreso tra 1-100 " + "</i></li>"
+      throw new Error("Input non valido");
+
+    }
+    else if (difficultyG == "Medium" && num > 80) {
+      document.getElementById("list").innerHTML += "<li><i>" + " Wrong Input " + num + " Deve essere compreso tra 1-80 " + "</i></li>"
+      throw new Error("Input non valido");
+
+    }
+    else if (difficultyG == "Hard" && num > 50) {
+      document.getElementById("list").innerHTML += "<li><i>" + " Wrong Input " + num + " Deve essere compreso tra 1-50 " + "</i></li>"
+      throw new Error("Input non valido");
+
+    }
+
+    if (isNaN(num) || /\s/.test(num)) {
+      console.log("Numero non valido!");
+    }
+    // Se il numero è gia stato inserito
+    else if (arrUser.includes(num)) {
+      alert("Numero già inserito!");
+      console.log(arrUser);
+    }
+    // Se il numero è presente nel campo minato della CPU
+    else if (uniqueSet.has(num)) {
+      console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
+      alert("End of the line dude!\nScore: " + score + String.fromCodePoint(0x1F4A3));
+      document.getElementById("btn3").className = "show";
+      console.log("Your score is " + score);
+    }
+
+    // Se il il punteggio raggiunge 5
+    else if (score >= 4) {
+     alert("Yeah, you win!!!" + String.fromCodePoint(0x1F91F));
+     document.getElementById("list").innerHTML += "<li><i>" + " &#127870 " + "</i></li>"
+     document.getElementById("btn3").className = "show";
+    }
+
+    // Se il numero è valido aumento il punteggio
+    else {
+      arrUser.push(num);
+      score += 1;
+      console.log(String.fromCodePoint(0x1F44C));
+      document.getElementById("list").innerHTML += "<li><i>" + "Mine missed &#128077 " + "</i></li>"
+    }
+}
 
 console.log("Minefield!");
 
 // Test con new Set
 var uniqueSet = new Set([]);
 
-var arrCPU = [];
 var arrUser = [];
 var score = 0;
-var numG;
 var difficultyG;
 
 
@@ -76,7 +113,6 @@ btn2.addEventListener("click", function(){
   document.getElementById("difficulty").className = "hide";
   document.getElementById("label1").className = "hide";
   document.getElementById("num").className = "show";
-  document.getElementById("btn").className = "show";
 
 
   if (difficulty == "Easy") {
@@ -100,54 +136,6 @@ btn2.addEventListener("click", function(){
   }
 
 })
-
-btn.addEventListener("click", function(){
-  console.log(score);
-  var num = parseInt(document.getElementById("num").value);
-    // Controllo che il numero sia valido
-    if (difficultyG == "Easy" && num > 100) {
-      throw new Error("Input non valido");
-    }
-    else if (difficultyG == "Medium" && num > 80) {
-      throw new Error("Input non valido");
-    }
-    else if (difficultyG == "Hard" && num > 50) {
-      throw new Error("Input non valido");
-    }
-
-
-    if (isNaN(num) || /\s/.test(num)) {
-      console.log("Numero non valido!");
-    }
-    // Se il numero è gia stato inserito
-    else if (arrUser.includes(num)) {
-      alert("Numero già inserito!");
-      console.log(arrUser);
-    }
-    // Se il numero è presente nel campo minato della CPU
-    else if (arrCPU.includes(num)) {
-      console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
-      alert("End of the line dude!\nScore: " + score + String.fromCodePoint(0x1F4A3));
-      document.getElementById("btn3").className = "show";
-      console.log("Your score is " + score);
-    }
-
-    // Se il il punteggio raggiunge 5
-    else if (score >= 4) {
-     alert("Yeah, you win!!!" + String.fromCodePoint(0x1F91F));
-     document.getElementById("list").innerHTML += "<li><i>" + " &#127870 " + "</i></li>"
-     document.getElementById("btn3").className = "show";
-    }
-
-    // Se il numero è valido aumento il punteggio
-    else {
-      arrUser.push(num);
-      score += 1;
-      console.log(String.fromCodePoint(0x1F44C));
-      document.getElementById("list").innerHTML += "<li><i>" + " &#128077 " + "</i></li>"
-    }
-
-});
 
 btn3.addEventListener("click", function(){
   location.reload();
