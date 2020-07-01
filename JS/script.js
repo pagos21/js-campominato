35e3bea5f3ffdd34dfa6fde80f3eba197004f24a
pagos21 <paga@localhost.yeah>
2020-07-01 17:12:20 +0200

    v 1.3

--- JS/script.js
+++ JS/script.js
@@ -5,17 +5,21 @@ function randomIntInclusiveF(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Il max è incluso e il min è incluso
 }
 
-// Check for duplicate numbers
-function duplicates(p1){
-  // Creo un nuovo Set dall'argomento(array) della funzione e confronto le loro lunghezze (TRUE se le lunghezze sono diverse).
-  return new Set(p1).size != p1.length;
+// funzione alternatica a includes
+function isDuplicated (array, p2) {
+  var i = 0;
+  var foundIt = false;
+  while (i < array.length && foundIt == false) {
+    if(array[i] == p2) {
+      foundIt = true;
+    }
+    i++;
+  }
+  return foundIt;
 }
 
 
 console.log("Minefield!");
-
-// Test con new Set
-var uniqueSet = new Set([]);
 
 var arrCPU = [];
 var arrUser = [];
@@ -23,31 +27,18 @@ var score = 0;
 
 for (var i = 0; (arrCPU.length != 16); i++) {
   var cpuNum = randomIntInclusiveF(1, 100);
-  if (arrCPU.includes(cpuNum)) {
-    continue;
-  } else {
-  arrCPU.push(cpuNum);
+  if (isDuplicated(arrCPU, cpuNum) != true) {
+    arrCPU.push(cpuNum);
   }
-  // uniqueSet.add(cpuNum);
 }
-
-
 console.log(arrCPU);
-// console.log(uniqueSet);
-if (duplicates(arrCPU)) {
-  throw new Error("Trovati dei doppioni nell'array della CPU \nQuitting...")
-}
-else {
-  console.log("Nessun doppione trovato.\nContinuo...");
-}
-console.log();
 
 
 btn.addEventListener("click", function(){
   var num = parseInt(document.getElementById("num").value);
 
   // Controllo che il numero sia valido
-  if (isNaN(num) || /\s/.test(num) || num > 100) {
+  if (isNaN(num) || num == "" || num > 100) {
     console.log("Numero non valido!");
   }
   // Se il numero è gia stato inserito
@@ -57,19 +48,19 @@ btn.addEventListener("click", function(){
   }
   // Se il numero è presente nel campo minato della CPU
   else if (arrCPU.includes(num)) {
-    console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
-    alert("End of the line dude!" + String.fromCodePoint(0x1F4A3));
+    console.log("Mine!!! ");
+    alert("End of the line dude!");
     console.log("Your score is " + score);
-    throw new Error("GAME OVER!")
+    console.log("GAME OVER!");
   }
-  // Se il il punteggio raggiunge 10
+  // Se il punteggio raggiunge 10
   else if (score >= 10) {
-    alert("Yeah, you win!!!" + String.fromCodePoint(0x1F91F))
+    alert("Yeah, you win!!!");
   }
   // Se il numero è valido aumento il punteggio
   else {
     arrUser.push(num);
     score += 1;
-    console.log(String.fromCodePoint(0x1F44C));
+    console.log("OK!");
   }
 });
