63adbe2c9c15e2568f137086eef4249d320f0964
pagos21 <paga@localhost.yeah>
2020-07-01 17:12:36 +0200

    v 2.3

--- BONUS/JS/script.js
+++ BONUS/JS/script.js
@@ -5,34 +5,52 @@ function randomIntInclusiveF(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Il max è incluso e il min è incluso
 }
 
+// funzione alternatica a includes
+function isDuplicated (array, p2) {
+  var i = 0;
+  var foundIt = false;
+  while (i < array.length && foundIt == false) {
+    if(array[i] == p2) {
+      foundIt = true;
+    }
+    i++;
+  }
+  return foundIt;
+}
+
 function easy(){
   console.log("Easy mode!");
-  for (var i = 0; (uniqueSet.size != 16); i++) {
+  for (var i = 0; (arrCPU.length != 16); i++) {
     var cpuNum = randomIntInclusiveF(1, 100);
-    uniqueSet.add(cpuNum);
+    if (isDuplicated(arrCPU, cpuNum) != true) {
+      arrCPU.push(cpuNum);
+    }
   }
-  console.log(uniqueSet);
+  console.log(arrCPU);
 
 }
 
 function medium(){
   console.log("Medium mode!");
-  for (var i = 0; (uniqueSet.size != 16); i++) {
+  for (var i = 0; (arrCPU.length != 16); i++) {
     var cpuNum = randomIntInclusiveF(1, 80);
-
-    uniqueSet.add(cpuNum);
+    if (isDuplicated(arrCPU, cpuNum) != true) {
+      arrCPU.push(cpuNum);
+    }
   }
-  console.log(uniqueSet);
+  console.log(arrCPU);
 
 }
 
 function hard(){
   console.log("Hard mode!");
-  for (var i = 0; (uniqueSet.size != 16); i++) {
+  for (var i = 0; (arrCPU.length != 16); i++) {
     var cpuNum = randomIntInclusiveF(1, 50);
-    uniqueSet.add(cpuNum);
+    if (isDuplicated(arrCPU, cpuNum) != true) {
+      arrCPU.push(cpuNum);
+    }
   }
-  console.log(uniqueSet);
+  console.log(arrCPU);
 
 }
 
@@ -45,7 +63,7 @@ btn2.addEventListener("click", function(){
   document.getElementById("btn2").className = "hide";
   document.getElementById("label1").className = "hide";
   document.getElementById("num").className = "show";
-
+  document.getElementById("btn").className = "show";
 
   if (difficulty == "Easy") {
     alert("Easy, OK!");
@@ -61,24 +79,12 @@ btn2.addEventListener("click", function(){
 
   else {
     hard();
-    var mediaClip = document.getElementById("OK");
-        mediaClip.play();
     alert("HARD, OK!");
     document.getElementById("num").placeholder = "Number 0-50";
   }
 
 })
 
-// Funzione per capire se l'utente ha premuto enter!
-function enterkey(){
-  if (event.keyCode === 13) {
-    enterPressed();
-    document.getElementById("num").value = "";
-
-  }
-}
-
-// Eseguo funzione se l'utente preme enter
 function enterPressed(){
   console.log(score);
   var list = document.getElementById("list");
@@ -87,33 +93,32 @@ function enterPressed(){
     // Controllo che il numero sia valido
     if (difficultyG == "Easy" && num > 100) {
       list.innerHTML += "<li>" + " Wrong Input " + num + " Deve essere compreso tra 1-100 " + "</li>"
-      throw new Error("Input non valido");
+      console.log("Input non valido");
 
     }
     else if (difficultyG == "Medium" && num > 80) {
       list.innerHTML += "<li>" + " Wrong Input " + num + " Deve essere compreso tra 1-80 " + "</li>"
-      throw new Error("Input non valido");
+      console.log("Input non valido");
 
     }
     else if (difficultyG == "Hard" && num > 50) {
       list.innerHTML += "<li>" + " Wrong Input " + num + " Deve essere compreso tra 1-50 " + "</li>"
-      throw new Error("Input non valido");
+      console.log("Input non valido");
 
     }
 
-    if (isNaN(num) || /\s/.test(num)) {
+    if (isNaN(num) || num == "") {
       console.log("Numero non valido!");
     }
     // Se il numero è gia stato inserito
-    else if (arrUser.includes(num)) {
+    else if (isDuplicated(arrUser, num)) {
       alert("Numero già inserito!");
       console.log(arrUser);
     }
     // Se il numero è presente nel campo minato della CPU
-    else if (uniqueSet.has(num)) {
-      console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
-      alert("End of the line dude!\nScore: " + score + String.fromCodePoint(0x1F4A3));
-      resetBtn.className = "show";
+    else if (isDuplicated(arrCPU, num)) {
+      console.log("Mine!!!");
+      alert("End of the line dude!\nScore: " + score);
       document.getElementById("num").className = "hide";
       list.innerHTML += "<li class='over'>" + " GAME OVER! " + "</li>"
       console.log("Your score is " + score);
@@ -121,32 +126,21 @@ function enterPressed(){
 
     // Se il il punteggio raggiunge 5
     else if (score >= 4) {
-     alert("Yeah, you win!!!" + String.fromCodePoint(0x1F91F));
+     alert("Yeah, you win!!!");
      list.innerHTML += "<li>" + " &#127870 " + "</li>"
-     resetBtn.className = "show";
     }
 
     // Se il numero è valido aumento il punteggio
     else {
       arrUser.push(num);
       score += 1;
-      console.log(String.fromCodePoint(0x1F44C));
+      console.log("OK!");
       list.innerHTML += "<li>" + "Mine missed &#128077 " + "</li>"
     }
 }
 
-
-// Reset Buton
-btn3.addEventListener("click", function(){
-  location.reload();
-});
-
-
 console.log("Minefield!");
-
-// Test con new Set
-var uniqueSet = new Set([]);
-
 var arrUser = [];
+var arrCPU = [];
 var score = 0;
 var difficultyG;
