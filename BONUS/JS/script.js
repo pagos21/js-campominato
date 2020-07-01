a3ec67edac013b492f6efd5e912f6658d7f409b6
pagos21 <paga@localhost.yeah>
2020-07-01 15:31:44 +0200

    v 2.4

--- BONUS/JS/script.js
+++ BONUS/JS/script.js
@@ -36,81 +36,13 @@ function hard(){
 
 }
 
-
-function enterkey(){
-  if (event.keyCode === 13) {
-    enterPressed();
-  }
-}
-
-function enterPressed(){
-  console.log(score);
-  var num = parseInt(document.getElementById("num").value);
-    // Controllo che il numero sia valido
-    if (difficultyG == "Easy" && num > 100) {
-      document.getElementById("list").innerHTML += "<li><i>" + " Wrong Input " + num + " Deve essere compreso tra 1-100 " + "</i></li>"
-      throw new Error("Input non valido");
-
-    }
-    else if (difficultyG == "Medium" && num > 80) {
-      document.getElementById("list").innerHTML += "<li><i>" + " Wrong Input " + num + " Deve essere compreso tra 1-80 " + "</i></li>"
-      throw new Error("Input non valido");
-
-    }
-    else if (difficultyG == "Hard" && num > 50) {
-      document.getElementById("list").innerHTML += "<li><i>" + " Wrong Input " + num + " Deve essere compreso tra 1-50 " + "</i></li>"
-      throw new Error("Input non valido");
-
-    }
-
-    if (isNaN(num) || /\s/.test(num)) {
-      console.log("Numero non valido!");
-    }
-    // Se il numero è gia stato inserito
-    else if (arrUser.includes(num)) {
-      alert("Numero già inserito!");
-      console.log(arrUser);
-    }
-    // Se il numero è presente nel campo minato della CPU
-    else if (uniqueSet.has(num)) {
-      console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
-      alert("End of the line dude!\nScore: " + score + String.fromCodePoint(0x1F4A3));
-      document.getElementById("btn3").className = "show";
-      console.log("Your score is " + score);
-    }
-
-    // Se il il punteggio raggiunge 5
-    else if (score >= 4) {
-     alert("Yeah, you win!!!" + String.fromCodePoint(0x1F91F));
-     document.getElementById("list").innerHTML += "<li><i>" + " &#127870 " + "</i></li>"
-     document.getElementById("btn3").className = "show";
-    }
-
-    // Se il numero è valido aumento il punteggio
-    else {
-      arrUser.push(num);
-      score += 1;
-      console.log(String.fromCodePoint(0x1F44C));
-      document.getElementById("list").innerHTML += "<li><i>" + "Mine missed &#128077 " + "</i></li>"
-    }
-}
-
-console.log("Minefield!");
-
-// Test con new Set
-var uniqueSet = new Set([]);
-
-var arrUser = [];
-var score = 0;
-var difficultyG;
-
-
+// Set difficulty
 btn2.addEventListener("click", function(){
   var difficulty = document.getElementById("difficulty").value;
   difficultyG = difficulty;
   console.log("Difficoltà settata a " + difficulty);
-  document.getElementById("btn2").className = "hide";
   document.getElementById("difficulty").className = "hide";
+  document.getElementById("btn2").className = "hide";
   document.getElementById("label1").className = "hide";
   document.getElementById("num").className = "show";
 
@@ -137,6 +69,83 @@ btn2.addEventListener("click", function(){
 
 })
 
+
+function enterkey(){
+  if (event.keyCode === 13) {
+    enterPressed();
+    document.getElementById("num").value = "";
+
+  }
+}
+
+function enterPressed(){
+  console.log(score);
+  var list = document.getElementById("list");
+  var num = parseInt(document.getElementById("num").value);
+  var resetBtn = document.getElementById("btn3");
+    // Controllo che il numero sia valido
+    if (difficultyG == "Easy" && num > 100) {
+      list.innerHTML += "<li>" + " Wrong Input " + num + " Deve essere compreso tra 1-100 " + "</li>"
+      throw new Error("Input non valido");
+
+    }
+    else if (difficultyG == "Medium" && num > 80) {
+      list.innerHTML += "<li>" + " Wrong Input " + num + " Deve essere compreso tra 1-80 " + "</li>"
+      throw new Error("Input non valido");
+
+    }
+    else if (difficultyG == "Hard" && num > 50) {
+      list.innerHTML += "<li>" + " Wrong Input " + num + " Deve essere compreso tra 1-50 " + "</li>"
+      throw new Error("Input non valido");
+
+    }
+
+    if (isNaN(num) || /\s/.test(num)) {
+      console.log("Numero non valido!");
+    }
+    // Se il numero è gia stato inserito
+    else if (arrUser.includes(num)) {
+      alert("Numero già inserito!");
+      console.log(arrUser);
+    }
+    // Se il numero è presente nel campo minato della CPU
+    else if (uniqueSet.has(num)) {
+      console.log("Mine!!! " + String.fromCodePoint(0x1F4A3));
+      alert("End of the line dude!\nScore: " + score + String.fromCodePoint(0x1F4A3));
+      resetBtn.className = "show";
+      document.getElementById("num").className = "hide";
+      list.innerHTML += "<li class='over'>" + " GAME OVER! " + "</li>"
+      console.log("Your score is " + score);
+    }
+
+    // Se il il punteggio raggiunge 5
+    else if (score >= 4) {
+     alert("Yeah, you win!!!" + String.fromCodePoint(0x1F91F));
+     list.innerHTML += "<li>" + " &#127870 " + "</li>"
+     resetBtn.className = "show";
+    }
+
+    // Se il numero è valido aumento il punteggio
+    else {
+      arrUser.push(num);
+      score += 1;
+      console.log(String.fromCodePoint(0x1F44C));
+      list.innerHTML += "<li>" + "Mine missed &#128077 " + "</li>"
+    }
+}
+
+
+
 btn3.addEventListener("click", function(){
   location.reload();
 });
+
+
+console.log("Minefield!");
+
+// Test con new Set
+var uniqueSet = new Set([]);
+
+var arrUser = [];
+var score = 0;
+var difficultyG;
